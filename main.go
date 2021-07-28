ca610b3322356f9cca9fa50c736b570af36b8a03
Sourav Verma <sourav@xage.com>
2021-07-28 11:21:39 -0700

    we are back

--- main.go
+++ main.go
@@ -2,8 +2,10 @@ package main
 
 import (
 	"fmt"
+
+	"github.com/aystzh/go-hello/hello/morestrings"
 )
 
 func main() {
-	fmt.Println("Habibi !")
+	fmt.Println(morestrings.ReverseRunes("!ibibah olleh"))
 }
