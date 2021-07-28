fdc274a70e4b022b3e147193c9653d8cc4a94c8f
Sourav Verma <sourav@xage.com>
2021-07-28 10:45:29 -0700

    added git ignore

--- main.go
+++ main.go
@@ -2,12 +2,8 @@ package main
 
 import (
 	"fmt"
-
-	"github.com/aystzh/go-hello/hello/morestrings"
-	"github.com/google/go-cmp/cmp"
 )
 
 func main() {
-	fmt.Println(morestrings.ReverseRunes("!oG ,olleH"))
-	fmt.Println(cmp.Diff("Hello World", "Hello Go"))
+	fmt.Println("Hello")
 }
